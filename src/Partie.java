9037d18720ab4f79c8859965d4b060f9b83fd57f
thib92 <thibhenry92@gmail.com>
2018-05-28 15:15:09 +0200

    Fixed Partie

--- src/Partie.java
+++ src/Partie.java
@@ -1,31 +1,14 @@
-import org.newdawn.slick.*;
-import javax.sound.sampled.*;
-
-import java.awt.*;
-import java.awt.Font;
-import java.io.File;
-import java.io.IOException;
-import org.newdawn.slick.Input;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-
 
 public class Partie {
 
-    ArrayList<Joueur> joueurs = new ArrayList<>();
+    private ArrayList<Joueur> joueurs = new ArrayList<>();
 
-    public Partie(ArrayList<Joueur> joueurs){
-        this.joueurs = joueurs;
-    }
-
-
-    public ArrayList<Joueur> nouvellePartie(int nbJoueurs){
-
+    public Partie(int nbJoueurs){
         for (int it = 1; it < nbJoueurs; it++){
             Joueur joueur = new Joueur();
             this.joueurs.add(joueur);
         }
-        return this.joueurs;
     }
 
 
