aa363bb5037f207e059492040c3d55ca60023c60
thib92 <thibhenry92@gmail.com>
2018-06-01 15:15:16 +0200

    Overloaded Boundary::contains

--- src/Graphics/Boundary.java
+++ src/Graphics/Boundary.java
@@ -1,5 +1,6 @@
 package Graphics;
 
+import org.newdawn.slick.geom.Point;
 import org.newdawn.slick.geom.Polygon;
 
 public class Boundary extends Polygon {
@@ -8,21 +9,25 @@ public class Boundary extends Polygon {
      * Return true if the given point is contained inside the boundary.
      * See: http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
      * See : https://stackoverflow.com/questions/8721406/how-to-determine-if-a-point-is-inside-a-2d-convex-polygon
-     * @param test The point to check
+     * @param point The point to check
      * @return true if the point is inside the boundary, false otherwise
      *
      */
-    public boolean contains(float[] test) {
+    public boolean contains(float[] point) {
         int i;
         int j;
         boolean result = false;
         for (i = 0, j = points.length - 1; i < points.length; j = i++) {
-            if ((this.getPoint(i)[1] > test[1]) != (this.getPoint(j)[1] > test[1]) &&
-                    (test[0] < (this.getPoint(j)[0] - this.getPoint(i)[0]) * (test[1] - this.getPoint(i)[1]) / (this.getPoint(j)[1]-this.getPoint(i)[1]) + this.getPoint(i)[0])) {
+            if ((this.getPoint(i)[1] > point[1]) != (this.getPoint(j)[1] > point[1]) &&
+                    (point[0] < (this.getPoint(j)[0] - this.getPoint(i)[0]) * (point[1] - this.getPoint(i)[1]) / (this.getPoint(j)[1]-this.getPoint(i)[1]) + this.getPoint(i)[0])) {
                 result = !result;
             }
         }
         return result;
+    }
+
+    public boolean contains(Point point) {
+        return this.contains(new float[] {point.getX(), point.getY()});
     }
 
 }
