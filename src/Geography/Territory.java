05a98e4adc28e843abbff7f61988101c7ac14944
thib92 <thibhenry92@gmail.com>
2018-06-01 15:12:13 +0200

    Neighbors on Territory

--- src/Geography/Territory.java
+++ src/Geography/Territory.java
@@ -2,15 +2,25 @@ package Geography;
 
 import Graphics.Boundary;
 
+import java.util.ArrayList;
+
 public class Territory {
 
 
     private String name;
     private Boundary boundary;
+    private ArrayList<Territory> neighbors;
 
     public Territory(String name, Boundary boundary) {
         this.name = name;
         this.boundary = boundary;
+        this.neighbors = new ArrayList<Territory>();
+    }
+
+    public Territory(String name, Boundary boundary, ArrayList<Territory> neighbors) {
+        this.name = name;
+        this.boundary = boundary;
+        this.neighbors = neighbors;
     }
 
     public String getName() {
@@ -27,5 +37,13 @@ public class Territory {
 
     public void setBoundary(Boundary boundary) {
         this.boundary = boundary;
+    }
+
+    public ArrayList<Territory> getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(ArrayList<Territory> neighbors) {
+        this.neighbors = neighbors;
     }
 }
