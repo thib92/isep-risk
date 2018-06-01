9abb97b407ea8ea7c116ae9bef516da5607efcf0
malobriant <malobriant@gmail.com>
2018-06-01 15:37:00 +0200

    map

--- src/Views/Map.java
+++ src/Views/Map.java
@@ -3,13 +3,18 @@ package Views;
 import Graphics.Drawable;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Map implements Drawable {
 
+
+    private Image map;
+
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
 
+        this.map = new Image("images/map.png");
 
     }
 
@@ -20,6 +25,8 @@ public class Map implements Drawable {
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException{
+
+        graphics.drawImage(this.map, 0, 0);
 
     }
 
